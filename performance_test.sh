3641595877ab950acca2350ba20661836d9becfc
Timofey Titovets <nefelim4ag@gmail.com>
2017-06-20 11:47:06 +0300

    Update test script
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

--- performance_test.sh
+++ performance_test.sh
@@ -12,7 +12,7 @@ echo "---"
 echo avg_meaning_entropy
 date
 for i in {1..10000}; do
-    ./avg_meaning_entropy > /dev/null
+    ./avg_meaning_entropy ./indata.bin > /dev/null
 done
 date
 
@@ -20,7 +20,7 @@ echo "---"
 echo shannon_entropy
 date
 for i in {1..10000}; do
-    ./shannon_entropy > /dev/null
+    ./shannon_entropy ./indata.bin > /dev/null
 done
 date
 
@@ -28,22 +28,22 @@ echo "---"
 echo shannon_int_entropy
 date
 for i in {1..10000}; do
-    ./shannon_int_entropy > /dev/null
+    ./shannon_int_entropy ./indata.bin > /dev/null
 done
 date
 
 echo "---"
-echo gzip -f -k -3 indata.bin
+echo gzip -f -k -3 ./indata.bin
 date
 for i in {1..10000}; do
-    gzip -f -k -3 indata.bin > /dev/null
+    gzip -f -k -3 ./indata.bin > /dev/null
 done
 date
 
 echo "---"
-echo lzop -f -k -3 indata.bin
+echo lzop -f -k -3 ./indata.bin
 date
 for i in {1..10000}; do
-    lzop -f -k -3 indata.bin > /dev/null
+    lzop -f -k -3 ./indata.bin > /dev/null
 done
 date
