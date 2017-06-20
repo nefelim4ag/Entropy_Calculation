a2c424e7b14421135b7e9aa9e33f91ca47509722
Timofey Titovets <nefelim4ag@gmail.com>
2017-06-20 18:08:50 +0300

    Make performance test more complex
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

--- performance_test.sh
+++ performance_test.sh
@@ -3,62 +3,89 @@
 cd "$(dirname $0)"
 
 # Gen test data
-{
-    dd if=/dev/urandom bs=64K count=1
-    dd if=/dev/zero    bs=64K count=1
-} > indata.bin
+rnd_zero_100_0(){ dd if=/dev/urandom bs=128K count=1 2> /dev/null; }
+rnd_zero_0_100(){ dd if=/dev/zero    bs=128K count=1 2> /dev/null; }
+rnd_zero_50_50(){
+    for i in {0..15}; do
+        dd if=/dev/urandom bs=4K count=1 2> /dev/null
+        dd if=/dev/zero    bs=4K count=1 2> /dev/null
+    done
+}
 
-{
-    echo "---"
-    echo avg_meaning_entropy
+avg_mean_test(){
+    echo -n "avg_meaning_entropy "
     START=$(date +%s)
     for i in {1..10000}; do
         ./avg_meaning_entropy ./indata.bin > /dev/null
     done
     STOP=$(date +%s)
-    echo $(($STOP-$START))s
+    echo "$(($STOP-$START))s"
 }
 
-{
-    echo "---"
-    echo shannon_entropy
+shannon_entropy(){
+    echo -n "shannon_entropy "
     START=$(date +%s)
     for i in {1..10000}; do
         ./shannon_entropy ./indata.bin > /dev/null
     done
     STOP=$(date +%s)
-    echo $(($STOP-$START))s
+    echo "$(($STOP-$START))s"
 }
 
-{
-    echo "---"
-    echo shannon_int_entropy
+shannon_int_entropy(){
+    echo -n "shannon_int_entropy "
     START=$(date +%s)
     for i in {1..10000}; do
         ./shannon_int_entropy ./indata.bin > /dev/null
     done
     STOP=$(date +%s)
-    echo $(($STOP-$START))s
+    echo "$(($STOP-$START))s"
 }
 
-{
-    echo "---"
-    echo gzip -f -k -3 ./indata.bin
+gzip_lvl_3(){
+    echo -n "gzip -f -k -3 ./indata.bin "
     START=$(date +%s)
     for i in {1..10000}; do
         gzip -f -k -3 ./indata.bin > /dev/null
     done
     STOP=$(date +%s)
-    echo $(($STOP-$START))s
+    echo "$(($STOP-$START))s"
 }
 
-{
-    echo "---"
-    echo lzop -f -k -3 ./indata.bin
+lzo_lvl_3(){
+    echo -n "lzop -f -k -3 ./indata.bin "
     START=$(date +%s)
     for i in {1..10000}; do
         lzop -f -k -3 ./indata.bin > /dev/null
     done
     STOP=$(date +%s)
-    echo $(($STOP-$START))s
+    echo "$(($STOP-$START))s"
 }
+
+echo "Test good compressible data"
+rnd_zero_0_100 > indata.bin
+avg_mean_test
+shannon_entropy
+shannon_int_entropy
+gzip_lvl_3
+lzo_lvl_3
+
+echo "- - - - -"
+
+echo "Test half compressible data"
+rnd_zero_50_50 > indata.bin
+avg_mean_test
+shannon_entropy
+shannon_int_entropy
+gzip_lvl_3
+lzo_lvl_3
+
+echo "- - - - -"
+
+echo "Test bad compressible data"
+rnd_zero_100_0 > indata.bin
+avg_mean_test
+shannon_entropy
+shannon_int_entropy
+gzip_lvl_3
+lzo_lvl_3
