4c39ad1c73db880b007ccd55d6d3c798c6ed8fbe
Timofey Titovets <nefelim4ag@gmail.com>
2017-06-20 21:09:40 +0300

    Use mmap instead of fread
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

--- avg_meaning_entropy.c
+++ avg_meaning_entropy.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <sys/mman.h>
 #include <fcntl.h>
 
 
@@ -9,29 +10,32 @@
 
 /* AVG Meaning calculation for file */
 int main(int argc, char *argv[]) {
-    int64_t sum = 0;
+    struct stat s;
+    uint64_t sum = 0;
     uint64_t count = 0;
     uint32_t i;
     int64_t avg, entropy;
-    int32_t accuracy = 100000; // 5 numbers after point
-    uint32_t input_data[BUCKET_SIZE];
-
-    FILE *file = fopen(argv[1], "rb");
-
-    if (!file) {
+    uint32_t accuracy = 100000; // 5 numbers after point
+    int64_t fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
         printf("Can't open file: %s\n", argv[1]);
         return 1;
     }
 
-    while (!feof(file)) {
-        fread(&input_data, BUCKET_SIZE, 4, file);
-        for (i = 0; i < BUCKET_SIZE; i++)
-            sum+=input_data[i];
-        count+=BUCKET_SIZE;
+    /* Get the size of the file. */
+    fstat (fd, & s);
+    uint32_t file_size = s.st_size;
+
+    uint32_t *input_data = (uint32_t *) mmap (0, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
+
+    for (i = 0; i < file_size/sizeof(uint32_t); i++) {
+        sum += input_data[i];
+        count++;
     }
-    fclose(file);
+
+    // close(file);
 
     avg = sum / count;
-    entropy = accuracy - abs(accuracy - avg*accuracy/(1 << 31)); // 0-100000
+    entropy = avg*accuracy/(1 << 31); // 0-100000
     printf("AVG Meaning: %li ~= Entropy lvl: %li\n", avg, entropy);
 }
--- shannon_int_entropy.c
+++ shannon_int_entropy.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <sys/mman.h>
 #include <fcntl.h>
 
 /* Precalculated log2 realization */
@@ -13,31 +14,36 @@
 /* Shannon integer entropy calculation */
 #define BUCKET_SIZE (1 << 8)
 int main(int argc, char *argv[]) {
+    struct stat s;
     uint64_t count = 0;
     uint64_t entropy_sum = 0;
     uint64_t entropy_l;
     double entropy_d;
     uint32_t i;
     /* Expected that: 4096 <= input data size <= 4294967296 */
+    uint32_t file_size;
     uint32_t *bucket = (uint32_t *) calloc(BUCKET_SIZE, sizeof(uint32_t));
-    /* Try add compiller some space for vectorization */
-    uint8_t input_data[BUCKET_SIZE];
-
-    FILE *file = fopen(argv[1], "rb");
-    if (!file) {
+    uint8_t *input_data;
+    int64_t fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
         printf("Can't open file: %s\n", argv[1]);
         return 1;
     }
 
+    /* Get the size of the file. */
+    fstat (fd, & s);
+    file_size = s.st_size;
 
-    while (!feof(file)) {
-        fread(&input_data, BUCKET_SIZE, 1, file);
-        for (uint16_t i = 0; i < BUCKET_SIZE; i++)
-            bucket[input_data[i]]++;
-        count+=BUCKET_SIZE;
+    input_data = (uint8_t *) mmap (0, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
+
+    for (i = 0; i < file_size; i++) {
+        bucket[input_data[i]]++;
     }
-    fclose(file);
 
+    // close(fd);
+
+    for (i = 0; i < BUCKET_SIZE; i++)
+        count += bucket[i];
 
     for (i = 0; i < BUCKET_SIZE; i++) {
         if (bucket[i]) {
