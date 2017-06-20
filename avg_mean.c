28590ce0de790b444fb968dfd96f263ec565adc7
Timofey Titovets <nefelim4ag@gmail.com>
2017-06-21 02:03:09 +0300

    Fixed: avg mean calculation
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

--- avg_mean.c
+++ avg_mean.c
@@ -1,24 +1,24 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "avg_mean.h"
 
 int avg_mean(uint8_t *input_data, uint64_t bytes_len) {
-    int64_t sum = 0;
-    uint64_t count = 0;
-    int64_t avg;
-    int64_t accuracy = 100000;
-    int64_t entropy;
-    uint32_t i;
+    uint64_t sum = 0;
+    uint64_t count = bytes_len/sizeof(uint32_t);
+    uint64_t avg;
+    int32_t accuracy = 100000;
+    int32_t entropy;
     uint32_t *_input_data = (uint32_t *) input_data;
 
-    count = bytes_len/sizeof(uint32_t);
-    for (i = 0; i < count; i++) {
+    for (uint32_t i = 0; i < count; i++) {
         sum += _input_data[i];
     }
 
     avg = sum*accuracy/count;
-    entropy = avg/2147483648; // 0-100000
-    printf("AVG Meaning: %li ~= Entropy lvl: %li\n", avg, entropy);
+    entropy = avg/2147483648; // avg vs expected avg for random data
+    entropy = accuracy - abs(accuracy - entropy);
+    printf("AVG Meaning: %lu ~= Entropy lvl: %i\n", avg, entropy);
 
     return 0;
 }
