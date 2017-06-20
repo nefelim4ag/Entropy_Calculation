00342026541370f9489a374752199f1718dc9af6
Timofey Titovets <nefelim4ag@gmail.com>
2017-06-20 12:16:11 +0300

    Update performance test script
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

--- performance_test.sh
+++ performance_test.sh
@@ -8,42 +8,57 @@ cd "$(dirname $0)"
     dd if=/dev/zero    bs=64K count=1
 } > indata.bin
 
-echo "---"
-echo avg_meaning_entropy
-date
-for i in {1..10000}; do
-    ./avg_meaning_entropy ./indata.bin > /dev/null
-done
-date
+{
+    echo "---"
+    echo avg_meaning_entropy
+    START=$(date +%s)
+    for i in {1..10000}; do
+        ./avg_meaning_entropy ./indata.bin > /dev/null
+    done
+    STOP=$(date +%s)
+    echo $(($STOP-$START))s
+}
 
-echo "---"
-echo shannon_entropy
-date
-for i in {1..10000}; do
-    ./shannon_entropy ./indata.bin > /dev/null
-done
-date
+{
+    echo "---"
+    echo shannon_entropy
+    START=$(date +%s)
+    for i in {1..10000}; do
+        ./shannon_entropy ./indata.bin > /dev/null
+    done
+    STOP=$(date +%s)
+    echo $(($STOP-$START))s
+}
 
-echo "---"
-echo shannon_int_entropy
-date
-for i in {1..10000}; do
-    ./shannon_int_entropy ./indata.bin > /dev/null
-done
-date
+{
+    echo "---"
+    echo shannon_int_entropy
+    START=$(date +%s)
+    for i in {1..10000}; do
+        ./shannon_int_entropy ./indata.bin > /dev/null
+    done
+    STOP=$(date +%s)
+    echo $(($STOP-$START))s
+}
 
-echo "---"
-echo gzip -f -k -3 ./indata.bin
-date
-for i in {1..10000}; do
-    gzip -f -k -3 ./indata.bin > /dev/null
-done
-date
+{
+    echo "---"
+    echo gzip -f -k -3 ./indata.bin
+    START=$(date +%s)
+    for i in {1..10000}; do
+        gzip -f -k -3 ./indata.bin > /dev/null
+    done
+    STOP=$(date +%s)
+    echo $(($STOP-$START))s
+}
 
-echo "---"
-echo lzop -f -k -3 ./indata.bin
-date
-for i in {1..10000}; do
-    lzop -f -k -3 ./indata.bin > /dev/null
-done
-date
+{
+    echo "---"
+    echo lzop -f -k -3 ./indata.bin
+    START=$(date +%s)
+    for i in {1..10000}; do
+        lzop -f -k -3 ./indata.bin > /dev/null
+    done
+    STOP=$(date +%s)
+    echo $(($STOP-$START))s
+}
