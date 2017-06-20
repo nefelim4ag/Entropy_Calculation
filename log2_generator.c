470cea7b936a5267d2c3a444a8bb6892ff5fa31a
Timofey Titovets <nefelim4ag@gmail.com>
2017-06-20 12:39:13 +0300

    log2_generator: Use fixed width integer types
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

--- log2_generator.c
+++ log2_generator.c
@@ -5,15 +5,20 @@
 #include <math.h>
 #include "log2_lshift16.h"
 
-const unsigned SIZE = 131072*8*1024;     // emulate 1024MB of input bytes
-const unsigned log_return_mpl = 1 << 6;  // << 6
-const unsigned fraction_shift = 1 << 16; // << 16
+/*
+ * Btrfs use 128Kb max block size for compress data
+ * So for more accuracy, lets precompute args for range:
+ * 1/1024*1024*1024 - 1024*1024*1024/1024*1024*1024
+ */
+const uint32_t SIZE = 1024*1024*1024;     // emulate 1024MB of input bytes
+const uint32_t log_return_mpl = 1 << 6;
+const uint32_t fraction_shift = 1 << 16;
 
 int main(int argc, char *argv[]){
-    int old_log_val = 0;
+    int32_t old_log_val = 0;
     bool gen = atoi(argv[1]);
-    unsigned i;
-    unsigned fraction_lshift16_old = 0;
+    uint32_t i;
+    uint32_t fraction_lshift16_old = 0;
     if (gen) {
         printf("/*\n");
         printf(" * Precalculated log2 values\n");
@@ -24,9 +29,9 @@ int main(int argc, char *argv[]){
         printf("int log2_lshift16(long long unsigned lshift16){\n");
         for (i = 1; i < SIZE; i++) {
             double fraction = (double) i/ (double) SIZE;
-            unsigned fraction_lshift16 = fraction*fraction_shift;
+            uint32_t fraction_lshift16 = fraction*fraction_shift;
 
-            int new_log_val = log2(fraction)*log_return_mpl;
+            int32_t new_log_val = log2(fraction)*log_return_mpl;
 
             if (old_log_val != new_log_val) {
                 if (fraction_lshift16 != fraction_lshift16_old) {
@@ -45,7 +50,7 @@ int main(int argc, char *argv[]){
         printf("fraction fraction*%u log2() == log2_lshift16()\n", fraction_shift);
         for (i = 1; i < SIZE; i++) {
             double fraction = (double) i/ (double) SIZE;
-            int new_log_val = log2(fraction)*log_return_mpl;
+            int32_t new_log_val = log2(fraction)*log_return_mpl;
             if (old_log_val != new_log_val) {
                 old_log_val = new_log_val;
                 printf("%i/%i %f %i == %i\n", i, SIZE, fraction*fraction_shift, new_log_val, log2_lshift16(fraction*fraction_shift));
