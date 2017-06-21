46ee23e6af2f5c996e6e97ce67f07ec5746ceb2c
Timofey Titovets <nefelim4ag@gmail.com>
2017-06-21 21:28:04 +0300

    Cleanup: shannon_i
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

--- shannon_i.c
+++ shannon_i.c
@@ -10,10 +10,10 @@
 #define BUCKET_SIZE 1 << 8
 
 int shannon_i(uint8_t *input_data, uint64_t bytes_len) {
-    uint64_t count = 0;
+    uint64_t count = bytes_len;
     uint64_t entropy_sum = 0;
-    uint64_t entropy_l;
-    double entropy_d;
+    uint64_t entropy_max = 8*LOG2_RET_SHIFT;
+    double entropy_perc;
     uint32_t i;
     /* Expected that: 4096 <= input data size <= 4294967296 */
     uint32_t *bucket = (uint32_t *) calloc(BUCKET_SIZE, sizeof(uint32_t));
@@ -21,21 +21,21 @@ int shannon_i(uint8_t *input_data, uint64_t bytes_len) {
     for (i = 0; i < bytes_len; i++)
         bucket[input_data[i]]++;
 
-    for (i = 0; i < BUCKET_SIZE; i++)
-        count += bucket[i];
-
     for (i = 0; i < BUCKET_SIZE; i++) {
         if (bucket[i]) {
-            entropy_l = bucket[i];
-            entropy_l = entropy_l*LOG2_ARG_SHIFT/count;
-            entropy_sum += -entropy_l*log2_lshift16(entropy_l);
+            uint64_t p = bucket[i];
+            p = p*LOG2_ARG_SHIFT/count;
+            entropy_sum += -p*log2_lshift16(p);
         }
     }
+
     free(bucket);
 
-    entropy_d = entropy_sum*100.0/LOG2_ARG_SHIFT/(8*LOG2_RET_SHIFT);
-    printf("Schanon int entropy: %lu/%u ~= %f%%\n",
-        entropy_sum/LOG2_ARG_SHIFT, 8*LOG2_RET_SHIFT ,entropy_d);
+    entropy_sum = entropy_sum/LOG2_ARG_SHIFT;
+    entropy_perc = entropy_sum*100.0/entropy_max;
+
+    printf("Schanon int entropy: %lu/%lu ~= %f%%\n",
+        entropy_sum, entropy_max, entropy_perc);
 
     return 0;
 }
