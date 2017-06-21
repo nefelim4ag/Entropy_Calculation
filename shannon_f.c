2e8ead55cc26acdf2b3645a550a4b89f46f2f3ad
Timofey Titovets <nefelim4ag@gmail.com>
2017-06-21 21:22:35 +0300

    Cleanup: shannon_f
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

--- shannon_f.c
+++ shannon_f.c
@@ -10,29 +10,26 @@
 
 int shannon_f(uint8_t *input_data, uint64_t bytes_len) {
     double entropy_sum = 0;
-    double entropy_l;
-    int64_t count = 0;
+    double entropy_max = 8;
+    uint64_t count = bytes_len;
     uint32_t i = 0;
     uint32_t *bucket = (uint32_t *) calloc(BUCKET_SIZE, sizeof(uint32_t));
 
     for (i = 0; i < bytes_len; i++)
         bucket[input_data[i]]++;
 
-    for (i = 0; i < BUCKET_SIZE; i++)
-        count += bucket[i];
-
     for (i = 0; i < BUCKET_SIZE; i++) {
         if (bucket[i]) {
-            entropy_l = bucket[i];
-            entropy_l = entropy_l/count;
-            entropy_sum += -(entropy_l)*log2(entropy_l);
+            double p = bucket[i];
+            p = p/count;
+            entropy_sum += -(p)*log2(p);
         }
     }
 
     free(bucket);
 
-    printf("Schanon true entropy: %f/8 ~= %f%%\n",
-        entropy_sum, entropy_sum*100/8);
+    printf("Schanon entropy: %f/%f ~= %f%%\n",
+        entropy_sum, entropy_max, entropy_sum*100/entropy_max);
 
     return 0;
 }
