c6a94f0b07ff21172aeafe0a5822066b88f55b21
Timofey Titovets <nefelim4ag@gmail.com>
2017-06-22 17:46:28 +0300

    Update: sort.h
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

--- sort.h
+++ sort.h
@@ -1,11 +1,5 @@
 #include <stdint.h>
 
-static int compare(const void *lhs, const void *rhs) {
-    int lhs_integer = *(const int *)(lhs);
-    int rhs_integer = *(const int *)(rhs);
-    return rhs_integer - lhs_integer;
-}
-
 void sort(
     void *base, uint64_t num, uint64_t size,
     int (*cmp_func)(const void *, const void *),
